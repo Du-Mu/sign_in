0c3fcb5345dea6cf056fdbbe14a2f5293b13c63d
nemo <wzhdxtx123@outlook.com>
2022-07-06 00:10:55 +0800

    edit script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # sign_in
 tri term sign_in scrpt
+
+
+## 使用
+
+编辑配置文件signin.yaml为自己的个人信息
+
+将脚本改为可执行，放入git仓库目录下
+
+把两个文件加入 .gitignore
+
+执行./sign_in.sh
+
+
+
+可以使用学长的check脚本进行配套检查
+
+
+
--- sign_in.sh
+++ sign_in.sh
@@ -1,11 +1,17 @@
-stu_id = $(cat signin.yaml | grep "stu_id:" | awk '{print $2}')
-data = $(cat signin.yaml | grep "data:" | awk '{print $2}')
-sign_key = $(cat signin.yaml | grep "data:" | awk '{print $2}')
-class_id= $(cat signin.yaml | grep "class_id:" | awk '{print $2}')
+#!/bin/bash
+cat signin.yaml
+stu_id=`cat signin.yaml | sed -n '1p' | awk '{print $2}' | tr -c -d '[0-9]'`
+mydata=`cat signin.yaml | sed -n '2p' | awk '{print $2}'| tr -c -d '[0-9] -'`
+sign_key=`cat signin.yaml | sed -n '3p' | awk '{print $2}'| tr -c -d '[0-9] [a-z] [A-Z]'`
+class_id=`cat signin.yaml | sed -n '4p' | awk '{print $2}'| tr -c -d '[0-9]'`
 repo_addr="https://se.jisuanke.com/whu-summer-course-2022/404"
 
+
 combined=$stu_id$sign_key$class_id
+touch $mydata
+echo -n $combined | md5sum | cut -d ' ' -f1 >$mydata
+#./check.sh
 
-echo -n $combined | md5sum  > $data
-
-./check.sh
+git add -A
+git commit -m "signin $mydata"
+git push origin $branch
